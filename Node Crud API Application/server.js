b1f34754b4f89e6d0e45ffe2e577e77262e59723
Alyunz12 <57860601+Alyunz12@users.noreply.github.com>
2024-03-01 23:21:49 +0300

    Add files via upload

---
+++ Node Crud API Application/server.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Product = require('./models/productModel')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.send('Hello CRUD NODE API')
+})
+
+app.get('/blog', (req, res) => {
+    res.send('Hello This Is NODE-API App')
+})
+
+app.get('/products', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/products/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/products', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.put('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product){
+            return res.status(404).json({message: `no product found with ID ${id}`})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.delete('/products/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `no product found with ID ${id}`})
+        }
+        res.status(200).json(product);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+mongoose.set("strictQuery", false)
+mongoose.
+connect('mongodb+srv://kipngetichvictor209:<victor1234>@apitechdevelopers.bjj6otd.mongodb.net/my-API?retryWrites=true&w=majority&appName=apitechdevelopers')
+.then(() => {
+    console.log('connected to database')
+    app.listen(5000, ()=> {
+        console.log(`Node API app is running on port 5000`)
+    });
+}).catch((error) => {
+    console.log(error)
+})
